c0087c72eda088aa6fb964d636eb25ce7de560bc
LUCAS S. OLIVEIRA <42159282+xtuser777@users.noreply.github.com>
2019-09-24 13:58:00 -0300

    Primeiro commit, adicionado o novo projeto com configuração inicial

---
+++ src/main/java/br/unoeste/fipp/scr/ScrTransgloboApplication.java
@@ -0,0 +1,13 @@
+package br.unoeste.fipp.scr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrTransgloboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrTransgloboApplication.class, args);
+	}
+
+}
